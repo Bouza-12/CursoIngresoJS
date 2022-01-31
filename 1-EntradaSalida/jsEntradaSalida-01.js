954a057d81c0bb5094b563acb68697d52b5b4f2d
Alejandro <ajgarciabouzada@gmail.com>
2022-01-31 17:33:28 -0300

    nuevo test 444

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Alejandro Garcia Bouzada");
+	alert("Alejandro Garcia 22222");
 }
 
